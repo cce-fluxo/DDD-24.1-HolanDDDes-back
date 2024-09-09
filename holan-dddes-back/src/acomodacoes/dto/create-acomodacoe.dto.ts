76fe712b7121cd097fbdcd89eeaaf8d515d37201
Gustavo-Roxo <146786288+Gustavo-Roxo@users.noreply.github.com>
2024-09-09 19:59:08 -0300

    Update create-acomodacoe.dto.ts
    falta revisar

--- holan-dddes-back/src/acomodacoes/dto/create-acomodacoe.dto.ts
+++ holan-dddes-back/src/acomodacoes/dto/create-acomodacoe.dto.ts
@@ -1,1 +1,17 @@
-export class CreateAcomodacoeDto {}
+export class CreateAcomodacoeDto {
+  titulo: string;
+  descricao?: string;
+  banheiros: number;
+  quartos: number;
+  camas: number;
+  valor_diaria: float;
+  valor_pet: float;
+  complemento: string;
+  tipo_acomodacaoId: number;
+  hotelId: number;
+  FotoAcomodacao: list;
+  Reserva: list;
+  Gerenciamento_ganhos: list;
+  Avaliacao_acomodacao: list;
+  ComodidadeNaAcomodacao: list;
+}
