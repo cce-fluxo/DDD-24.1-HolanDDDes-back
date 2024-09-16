5353a8bdcefe4b79f6f0415255b2ed7a9481cf1c
Kauã Gomes <70298794+Kauagpaiva@users.noreply.github.com>
2024-09-16 19:30:33 -0300

    update temporario PrismaService

--- holan-dddes-back/src/database/prisma.service.ts
+++ holan-dddes-back/src/database/prisma.service.ts
@@ -6,6 +6,27 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   acomodacoe: any;
   avaliacao: any;
   avaliacaoAcomodacoe: any;
+  usuario: any;
+  tipoAcomodacoe: any;
+  reserva: any;
+  proprietario: any;
+  notificacao: any;
+  interesse: any;
+  hotei: any;
+  gerenciamentoGanhoAcomodacao: any;
+  gerenciamentoGanho: any;
+  fotosHotei: any;
+  fotosAcomodacoe: any;
+  fotoUsuario: any;
+  favorito: any;
+  endereco: any;
+  descricaoDetalhada: any;
+  cupom: any;
+  comodidadesHotei: any;
+  comodidadeAcomodacoe: any;
+  cobranca: any;
+  cliente: any;
+
   async onModuleInit() {
     await this.$connect();
   }
