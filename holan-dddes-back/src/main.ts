8e4c6c949f4b6df49b6c30b2c8735147e227a234
Aishabrito <138252961+Aishabrito@users.noreply.github.com>
2024-09-17 14:33:02 -0300

    Ajeitando a porta

--- holan-dddes-back/src/main.ts
+++ holan-dddes-back/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'localhost:3300',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   await app.listen(3300);
 }
+
 bootstrap();
