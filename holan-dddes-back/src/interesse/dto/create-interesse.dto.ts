e0b4772f6c751675f55d6c2aed316815436034e6
Kauã Gomes <70298794+Kauagpaiva@users.noreply.github.com>
2024-09-29 15:08:50 -0300

    class validator rosa

--- holan-dddes-back/src/interesse/dto/create-interesse.dto.ts
+++ holan-dddes-back/src/interesse/dto/create-interesse.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateInteresseDto {
   @ApiProperty({
     description: "Nome do interesse",
     example: "Voley"
   })
+  @IsString()
+  @IsNotEmpty()
   Nome: string;
 }
--- holan-dddes-back/src/notificacao/dto/create-notificacao.dto.ts
+++ holan-dddes-back/src/notificacao/dto/create-notificacao.dto.ts
@@ -1,33 +1,44 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsDate, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class CreateNotificacaoDto {
   @ApiProperty({
     description: "Mensagem que será exibida na notificação",
     example:"Fulano acabou de se hospedar no seu hotel XXX",
   })
-  mensagem: string;
+  @IsString()
+  @IsOptional()
+  mensagem?: string;
 
   @ApiProperty({
     description: "Títulod e notificação.",
     example:"Novo Hospede",
   })
+  @IsString()
+  @IsNotEmpty()
   titulo: string;
 
   @ApiProperty({
     description: "ID único do usuário que receberá a notficação",
     example:"123456789",
   })
+  @IsNotEmpty()
+  @IsNumber()
   usuarioId: number;
 
   @ApiProperty({
     description: "Registra se a notificação foi ou não aberta",
     example:"false",
   })
-  leitura: boolean;
+  @IsBoolean()
+  @IsOptional()
+  leitura?: boolean;
 
   @ApiProperty({
     description: "Registra a data em que a notificação foi criada",
     example:"2024-01-01T00:00:00Z",
   })
-  data_criacao: Date;
+  @IsDate()
+  @IsOptional()
+  data_criacao?: Date;
 }
