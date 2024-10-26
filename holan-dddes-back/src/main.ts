f62fef42c1ede63a81fc7644f61df476a8d7adfa
Mariana Fonseca <118086356+fonsecamc@users.noreply.github.com>
2024-10-26 10:08:13 -0300

    Update main.ts

--- holan-dddes-back/src/main.ts
+++ holan-dddes-back/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.enableCors({
-    origin: 'localhost:3300',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
