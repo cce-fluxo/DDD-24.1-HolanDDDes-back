9d2b693df3a270e43dbcd2c1f396f945fa4f6694
coderatta <coderatta@gmail.com>
2024-11-05 20:14:52 -0300

    add get descricao_detalhada

--- holan-dddes-back/src/descricao_detalhada/descricao_detalhada.service.ts
+++ holan-dddes-back/src/descricao_detalhada/descricao_detalhada.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
 import { CreateDescricaoDetalhadaDto } from './dto/create-descricao_detalhada.dto';
 import { UpdateDescricaoDetalhadaDto } from './dto/update-descricao_detalhada.dto';
 import { PrismaService } from '../database/prisma.service';
@@ -6,9 +11,53 @@ import { PrismaService } from '../database/prisma.service';
 @Injectable()
 export class DescricaoDetalhadaService {
   constructor(private prisma: PrismaService) {}
-  create(createDescricaoDetalhadaDto: CreateDescricaoDetalhadaDto) {
+
+  // Automatizando a adição de acomodações pelo FRONT
+  // com essa function não preciso fazer mais nada de adicional no FRONT
+  async getProprietarioId(userId: number) {
+    try {
+      const proprietario = await this.prisma.proprietario.findFirst({
+        where: { usuarioId: userId },
+      });
+      if (!proprietario) {
+        throw new BadRequestException('Usuário não encontrado.');
+      }
+
+      return proprietario.id;
+    } catch (error) {
+      throw new HttpException(
+        `Erro ao buscar o proprietário: ${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async getHotelId(userId: number) {
+    try {
+      const hotel = await this.prisma.hotel.findFirst({
+        where: { proprietarioId: await this.getProprietarioId(userId) },
+      });
+      if (!hotel) {
+        throw new BadRequestException(
+          'Usuário não está associado a nenhum hotel.',
+        );
+      }
+
+      return hotel.id;
+    } catch (error) {
+      throw new HttpException(
+        `Erro ao buscar o hotel: ${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+  async create(
+    createDescricaoDetalhadaDto: CreateDescricaoDetalhadaDto,
+    userId: number,
+  ) {
+    const hotelId = await this.getHotelId(userId);
     const CriarDescricaoDetalhada = this.prisma.descricao_detalhada.create({
-      data: createDescricaoDetalhadaDto,
+      data: { ...createDescricaoDetalhadaDto, hotelId: hotelId },
     });
     return CriarDescricaoDetalhada;
   }
@@ -22,18 +71,18 @@ export class DescricaoDetalhadaService {
   }
 
   async findOne(id: number) {
-    return await this.prisma.descricao_detalhada.findUnique({where: {id}});
+    return await this.prisma.descricao_detalhada.findUnique({ where: { id } });
   }
 
   update(id: number, UpdateDescricaoDetalhadaDto: UpdateDescricaoDetalhadaDto) {
     const descricaoDetalhada = this.prisma.descricao_detalhada.update({
-      where: {id},
+      where: { id },
       data: UpdateDescricaoDetalhadaDto,
-    })
-    return descricaoDetalhada
+    });
+    return descricaoDetalhada;
   }
 
   async remove(id: number) {
-    return await this.prisma.descricao_detalhada.delete({where: {id}});
+    return await this.prisma.descricao_detalhada.delete({ where: { id } });
   }
 }
